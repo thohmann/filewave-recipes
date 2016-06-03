3b9bb673476e53e2d93527f70973ddee24b2c28d
Timo Hohmann <timo.hohmann@burda.com>
2016-06-03 14:19:35 +0200

    modified:   Slack/PathCHOWN.py

--- Slack/PathCHOWN.py
+++ Slack/PathCHOWN.py
@@ -32,15 +32,12 @@ class PathCHOWN(Processor):
 		group = self.env.get('group')
 		if os.path.exists(path):
 			owner = user + ':' + group
-			cmd = ['sudo', 'chown', '-R', owner, path]
-			proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			
-			'''for root, dirs, files in os.walk(path):
-				for momo in dirs:
-					os.lchown(os.path.join(root, momo), int(os.getenv('SUDO_UID')), int(os.getenv('SUDO_UID')))
-				for momo in files:
-					os.lchown(os.path.join(root, momo), int(os.getenv('SUDO_UID')), int(os.getenv('SUDO_UID')))'''
-	
+			retcode = subprocess.call([['sudo', '/bin/chown', '-R', owner, path]])
+			if retcode:
+				raise ProcessorError('Error setting owner (chown %s) for %s' % (owner, path))
+
+			return
+				
 if __name__ == "__main__":
 	PROCESSOR = PathCHOWN()
 	PROCESSOR.execute_shell()
