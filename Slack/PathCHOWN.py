1f4576614aa89027ce7da91d086918f99abf1366
Timo Hohmann <timo.hohmann@burda.com>
2016-06-03 09:22:52 +0200

    new file:   Slack/PathCHOWN.py

---
+++ Slack/PathCHOWN.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from autopkglib import Processor, ProcessorError
+import os
+
+__all__ = ["PathCHOWN"]
+
+class PathCHOWN(Processor):
+	'''Changes path owner'''
+
+	input_variables = {
+		'path': {
+			'required': True,
+			'description': 'Name of filename resource',
+		},
+		'userid': {
+			'required': True,
+			'description': 'chown uid E.g. "0,501,..."'
+		},
+		'groupid': {
+			'required': True,
+			'description': 'chown gid. E.g. "0,20,80,..."'
+		},
+	}
+	output_variables = {
+	}
+
+	def main(self):
+		path = self.env.get('path')
+		uid = self.env.get('userid')
+		gid = self.env.get('groupid')
+		if os.path.exists(path):
+			for root, dirs, files in os.walk(path):
+				for momo in dirs:
+					os.lchown(os.path.join(root, momo), uid, gid)
+				for momo in files:
+					os.lchown(os.path.join(root, momo), uid, gid)
+	
+if __name__ == "__main__":
+	PROCESSOR = PathCHOWN()
+	PROCESSOR.execute_shell()
