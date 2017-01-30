745845f6fac8b5c6d233cf15c0f9568f865e7b61
Timo Hohmann <timo.hohmann@burda.com>
2017-01-30 14:59:54 +0100

    deleted spaces

--- Processors/PathCleaner.py
+++ Processors/PathCleaner.py
@@ -20,12 +20,12 @@ class PathCleaner(Processor):
 	def main(self):
 		folder_path = self.env.get('path')
 		for file_object in os.listdir(folder_path):
-    		file_object_path = os.path.join(folder_path, file_object)
-   			if os.path.isfile(file_object_path):
-        		os.unlink(file_object_path)
-    		else:
-        		shutil.rmtree(file_object_path)
-				
+			file_object_path = os.path.join(folder_path, file_object)
+			if os.path.isfile(file_object_path):
+				os.unlink(file_object_path)
+			else:
+				shutil.rmtree(file_object_path)
+
 if __name__ == "__main__":
 	PROCESSOR = PathCleaner()
 	PROCESSOR.execute_shell()
